a10ca2881ca17fed1ef3a38174876567bd232502
Alhajideen <salifunurudeen834@gmail.com>
2022-09-19 10:45:46 -0600

    add an event listener on the form and create an object on submit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
 const form = document.querySelector('.book-form');
+const bookTittle = document.querySelector('.tittle');
+const bookAuth = document.querySelector('.boot-author');
+const bookArr = [];
+
+form.addEventListener('submit', () => {
+  const book = {
+    tittl: bookTittle.value,
+    author: bookAuth.value,
+  };
+  bookArr.push(book);
+});
+
+const displaytoDom = () => {};
